0f560531b3715fd6a53aefeb1b0ba7055eef4a23
Valery Dolm <valery.pisannyh@gmail.com>
2021-01-20 17:30:30 +0300

    Adder some changes in code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let sort = document.querySelector('.container__inputSort');
 let move = document.querySelector('.container__movebuttone');
 let input = document.querySelector('.input');
-let remove = document.querySelector('.container_deleteNote');
+let remove = document.querySelector('.container__deleteNote');
 let addField = document.querySelector('.container__cross');
 let field = document.querySelector('.container__inputField');
 
@@ -15,15 +15,34 @@ addField.addEventListener(('click'), () => {
     xLogo.addEventListener(('click'), () => { 
         newElem.remove();
     })
-})
+});
   
+remove.addEventListener('click', () => {
+    input.remove();
+});
+
+
+/*Drag&Drops определеяем перетаскиваемые элементы*/
+let items = document.querySelectorAll('.input');
+
+items.forEach((input) => {
+    field.addEventListener('dragstart', (event) => {
+        event.target.style.backgroundColor = 'red';
+    });    
+    field.addEventListener('dragend', (event) => {
+        event.target.style.backgroundColor = 'unset';    
+    });
+});
 
 
 
-
-
+items.addEventListener(`dragenter`, (evt) => {
     
+  })
+  
+  items.addEventListener(`dragleave`, (evt) => {
+    
+  });
 
-
-
-
+  field.addEventListener(`dragover`, (evt) => {
+    evt.preventDefault();
